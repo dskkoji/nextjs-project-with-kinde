bbcb5bd5ab7570e89cbf613254d8fdc6d4488e80
dskkoji <50835363+dskkoji@users.noreply.github.com>
2024-08-08 00:34:25 +0900

    modified

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware'
 import { NextResponse } from 'next/server'
 
 export default function middleware(req: NextResponse) {
-  return withAuth()
+  return withAuth(req)
 }
 
 export const config = {
